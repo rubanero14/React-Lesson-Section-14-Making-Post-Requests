841bcd3342ffa8c6237778820a68436427953ab7
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-04-07 04:33:35 +0800

    Preparing The Project For The Next Steps

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-certification-68c63-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
